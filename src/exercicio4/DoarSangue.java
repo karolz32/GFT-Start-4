7bf98a5b2593b5f17bb67f92ada0c65f1ab70348
karolz32 <karoline.lima32@gmail.com>
2022-03-31 17:41:12 -0300

    add exercicio4

--- src/exercicio4/DoarSangue.java
+++ src/exercicio4/DoarSangue.java
@@ -1,4 +1,22 @@
 package exercicio4;
 
+import java.util.Scanner;
+
 public class DoarSangue {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe sua idade: ");
+        int idade = sc.nextInt();
+
+        if (idade >= 18 && idade <= 67){
+            System.out.println("Parabéns! Você pode ser um doador de sangue!");
+        }else {
+            System.out.println("Sinto muito, mas você não poderá doar.");
+        }
+
+        sc.close();
+    }
 }
